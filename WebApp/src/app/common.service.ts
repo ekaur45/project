28255dff6a4d36d7f96cd013dd5b36165eb9b6b2
WAQASAHMAD <WAQASAHMAD@IT-33>
2018-03-07 21:58:07 +0500

    commit

--- WebApp/src/app/common.service.ts
+++ WebApp/src/app/common.service.ts
@@ -1,16 +1,18 @@
 import { Injectable } from '@angular/core';
 import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { Http, Response } from '@angular/http'
 import { Data } from './models/Data'
 import { Observable } from 'rxjs/Observable';
 import { of } from 'rxjs/observable/of';
 import { catchError, map, tap } from 'rxjs/operators';
+import 'rxjs/Rx'
 
 @Injectable()
 export class CommonService {
-    constructor(private http: HttpClient) { }
-    GetData(): Observable<Data[]> {
-        return this.http.get<Data[]>('http://localhost:7712/src/assets/Data.json').pipe(
-            tap(heroes => console.error(`fetched heroes`))            
-        );
+    constructor(private http: Http) { }
+    GetData():any{
+        return this.http.get('http://localhost:7712/src/assets/Data.json').map(res => {
+            res.json()
+        });
     }
 }
